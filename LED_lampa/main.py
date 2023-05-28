9ca6d5c3f40be0f5dc557ef0014cf4b8c8b5c2cc
JakubDanihel <108288883+JakubDanihel@users.noreply.github.com>
2023-05-28 20:32:40 +0200

    Add files via upload

---
+++ LED_lampa/main.py
@@ -0,0 +1,32 @@
+from machine import Pin
+import time
+
+led = Pin(15, Pin.OUT)                   
+button = Pin(13, Pin.IN, Pin.PULL_UP)    #Create button object from Pin13 , Set GP13 to input
+
+#vytvorenie kyvadlovej funkcie pre zmenu stavu LED na inu hodnotu voci hodnote ktoru malo ako poslednu
+def reverseGPIO():
+    if led.value():
+        led.value(0)
+    else:
+        led.value(1)
+        
+#funkcia pre spustanie tlacidla a overenie ci je tlacidlo stlacne znova pre vypnutie svetla
+def run():
+    #ak nie je hodnota pre tlacidlo 1 tak je svetlo pre tuto poziciu na zaciatku zhasnute
+    if not button.value():
+        time.sleep_ms(20)
+        #ked dojde k zmene stavu svetla na tlacidlu tak sa spusti reverseGPIO() azmeni sa hodnota led na to aby svietila albo zhasla podla toho co je posledna hodnota
+        if button.value():
+            reverseGPIO()
+            while button.value():
+                time.sleep_ms(20)
+
+#spusenie kodu
+if __name__ == "__main__":
+    try:
+        while True:
+            run()
+    except:
+        pass
+
